630ddb541cfb0eaecf2def09081a0577d0a0704f
lexus2727 <lexus2424@yahoo.com>
2020-08-13 20:14:56 -0400

    Updates

---
+++ precache-manifest.a2828202ece15bd3e52e41b32a1836be.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5f4f9ecfd5d2bb4b27ad218a12a0b8",
+    "url": "/temperature_converter/index.html"
+  },
+  {
+    "revision": "d840406697a299de580a",
+    "url": "/temperature_converter/static/js/main.882e1394.chunk.js"
+  },
+  {
+    "revision": "c053f31a9fe4f38c870b",
+    "url": "/temperature_converter/static/js/runtime-main.8bab2081.js"
+  }
+]);
---
+++ static/js/main.882e1394.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-temp"]=this["webpackJsonpmy-temp"]||[]).push([[0],[function(p,t,n){p.exports=n(1)},function(p,t){}],[[0,1]]]);
+//# sourceMappingURL=main.882e1394.chunk.js.map
